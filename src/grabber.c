68514151d3126847838d572d2faeade646e69345
ethanxxxl <ethansmith.dev@gmail.com>
2017-09-27 16:43:25 -0400

    added a template for the grabber

---
+++ src/grabber.c
@@ -0,0 +1,46 @@
+#include "main.h"
+#include "grabber.h"
+
+/*
+this is a template that needs filled 
+*/
+
+void autoOpen(int channel)
+{
+
+}
+
+void autoClose(int channel)
+{
+
+}
+
+void open(int channel, int value)
+{
+
+}
+
+void close(int channel, int value)
+{
+
+}
+
+void autoUp(int channel)
+{
+
+}
+
+void autoDown(int channel)
+{
+
+}
+
+void up(int channel, int value)
+{
+
+}
+
+void down(int channel, int value)
+{
+
+}
--- src/grabber.h
+++ src/grabber.h
@@ -1,9 +1,16 @@
 #ifndef _GRABBER_H_
 #define _GRABBER_H_
 
-void close(int channel);
+void autoOpen(int channel);
+void autoClose(int channel);
+
+void open(int channel, int value);
+void close(int channel, int value);
 
 void autoUp(int channel);
 void autoDown(int channel);
+
+void up(int channel, int value);
+void down(int channel, int value);
 
 #endif
